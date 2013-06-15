dfc5dd79900c92f9d77c13372ad93eacfe0cc02e
ccockerhamkc <curtis.cockehram@gmail.com>
2013-06-15 18:04:04 -0500

    Twilio updated

--- HackMW2013/HackMW2013/Models/Twilio.cs
+++ HackMW2013/HackMW2013/Models/Twilio.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using RestSharp;
+using Twilio;
 
 namespace HackMW2013.Models
 {
     public class Twilio
     {
-        string accountSID = "AC7dbe24fc85a63e2ac866490e9948cd44";
-        public void SendSMS(string phoneNumber, string messageBody)
+        private const string AccountSid = "AC5160b29cfe7044fb634bfe449a47a0c8";
+        private const string AuthToken = "17f74b6c0648f4feabc0a8844fa6665a";
+
+        public void SendSms(string accountNumber, string outgoingNumber)
         {
-            var client = new RestClient(@"https://");
+            var client = new TwilioRestClient(AccountSid, AuthToken);
+            client.SendSmsMessage(accountNumber, outgoingNumber, "Texting Works this way.");
         }
     }
 }
