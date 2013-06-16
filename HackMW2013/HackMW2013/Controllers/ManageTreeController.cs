63169930eeca6740f5969b26cb1fc7b8598bbd60
Josh Waldron <jwaldron@thejwal.com>
2013-06-15 19:48:56 -0500

    added manageTree mockup

---
+++ HackMW2013/HackMW2013/Controllers/ManageTreeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackMW2013.Controllers
+{
+    public class ManageTreeController : Controller
+    {
+        //
+        // GET: /ManageTree/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
